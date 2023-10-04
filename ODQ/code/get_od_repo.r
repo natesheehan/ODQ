1acc93cb0faa6c5522120b47afc965c149573149
Sheehan <nathanaelsheehan@gmail.com>
2023-10-04 15:42:49 +0100

    update file folders

---
+++ ODQ/code/get_od_repo.r
@@ -0,0 +1,27 @@
+###########################################################################
+###########################################################################
+###                                                                     ###
+###                 BUILD DATAFRAME OF RE3 REPOSITOIRES                 ###
+###                                                                     ###
+###########################################################################
+###########################################################################
+# Step 1: Fetch XML content from the URL
+re3_url_repos = "https://www.re3data.org/api/v1/repositories"
+re3_url_repo = "https://www.re3data.org/api/v1/repository/"
+
+# Step 2: Parse data from URL
+raw_data = xml2::read_xml(re3_url_repos)
+
+# Step 3: Collect Name and ID
+name = xml2::xml_text(xml2::xml_find_all(raw_data, xpath = "//name"))
+id = xml2::xml_text(xml2::xml_find_all(raw_data, xpath = "//id"))
+
+# Step 4: Merge into table
+re3_ids = tibble::tibble(name = name, id = id)
+
+
+
+test = read_xml(paste0(re3_url_repo,re3_ids$id[69]))
+
+xml_text(xml_find_all(test, xpath = "//r3d:additionalName"))
+xml_text(xml_find_all(test, xpath = "//r3d:dataAccessType"))
---
+++ ODQ/code/setup.r
@@ -0,0 +1,15 @@
+pacman = function(pkg) {
+  new.pkg = pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+pkgs = c(
+  "xml2",
+  "tibble"
+)
+
+options(scipen = 999) # Turn off scientific notation
+pacman(pkgs)
+rm(pkgs)
