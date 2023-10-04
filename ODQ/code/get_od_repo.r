755fe4091ab5e5620fd89efc4d5a7d2951659bd4
Sheehan <nathanaelsheehan@gmail.com>
2023-10-04 19:21:22 +0100

    update scripts

--- ODQ/code/get_od_repo.r
+++ ODQ/code/get_od_repo.r
@@ -22,16 +22,30 @@ re3_ids = tibble::tibble(name = name, id = id)
 # Step 5: Save data
 saveRDS(re3_ids,"data/re3_ids.RDS")
 
-for(i in 1:nrow(re3_ids)) {
-  print(paste0("OK! HERE WE GO. Printing for ", re3_ids$name[i]))
-  repo_data = read_xml(paste0(re3_url_repo,re3_ids$id[i]))
+# Step 6: Set up core cluster
+num_cores <- detectCores() - 1
+cl <- makeCluster(num_cores)
+clusterExport(cl, list("re3_ids", "re3_url_repo", "read_xml", "xml_text", "xml_find_all"))
+clusterEvalQ(cl, library(xml2))
+
+# Step 7: Create Function to process metadata
+process_id <- function(i) {
+  cat(paste0("OK! HERE WE GO. Printing for ", re3_ids$name[i]), "\n")
+
+  repo_data = read_xml(paste0(re3_url_repo, re3_ids$id[i]))
   repo_names = xml_text(xml_find_all(repo_data, xpath = "//r3d:additionalName"))
-  repo_access = xml_text(xml_find_all(repo_data, xpath = "//r3d:dataAccessType"))
-  re3_ids$names[i] = list(repo_names)
-  re3_ids$access[i] = repo_access
+  repo_access = xml_text(xml_find_all(repo_data, xpath = "//r3d:databaseAccessType"))
+
+  list(
+    access = repo_access,
+    names = paste(repo_names, collapse = ",")
+  )
 }
 
-
-
-
+# Step 8 Merge Data
+results <- parLapply(cl, 1:nrow(re3_ids), process_id)
+for (i in 1:nrow(re3_ids)) {
+  re3_ids$access[i] = results[[i]]$access
+  re3_ids$names[i] = results[[i]]$names
+}
 
